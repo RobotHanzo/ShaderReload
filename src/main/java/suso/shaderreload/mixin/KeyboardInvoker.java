77d8ae34ad612166c4a4b1d4b027cea750c97f21
Suso <jesusmosteiro@gmail.com>
2024-11-07 12:31:14 +0100

    Update keyboard mixin

--- src/main/java/suso/shaderreload/mixin/KeyboardInvoker.java
+++ src/main/java/suso/shaderreload/mixin/KeyboardInvoker.java
@@ -1,10 +1,12 @@
 package suso.shaderreload.mixin;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-@Mixin(Keyboard.class)
+@Mixin(Keyboard.class) @Environment(EnvType.CLIENT)
 public interface KeyboardInvoker {
     @Invoker
     void invokeDebugLog(String key, Object ... args);
--- src/main/java/suso/shaderreload/mixin/KeyboardMixin.java
+++ src/main/java/suso/shaderreload/mixin/KeyboardMixin.java
@@ -1,5 +1,7 @@
 package suso.shaderreload.mixin;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.InputUtil;
@@ -14,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import suso.shaderreload.ShaderReload;
 
-@Mixin(Keyboard.class)
+@Mixin(Keyboard.class) @Environment(EnvType.CLIENT)
 public abstract class KeyboardMixin {
     @Shadow @Final private MinecraftClient client;
 
@@ -33,9 +35,7 @@ public abstract class KeyboardMixin {
         }
     }
 
-    @Inject(method = "onKey", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/screen/Screen;wrapScreenError(Ljava/lang/Runnable;Ljava/lang/String;Ljava/lang/String;)V"),
-            cancellable = true)
+    @Inject(method = "onKey", at = @At(value = "HEAD"), cancellable = true)
     void onOnKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
         if (!InputUtil.isKeyPressed(window, GLFW.GLFW_KEY_F3) || key != ShaderReload.GLFW_KEY) return;
         if (action != 0) {
@@ -44,9 +44,7 @@ public abstract class KeyboardMixin {
         ci.cancel();
     }
 
-    @Inject(method = "onChar", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/screen/Screen;wrapScreenError(Ljava/lang/Runnable;Ljava/lang/String;Ljava/lang/String;)V",
-            ordinal = 0), cancellable = true)
+    @Inject(method = "onChar", at = @At(value = "HEAD"), cancellable = true)
     void onOnChar(long window, int codePoint, int modifiers, CallbackInfo ci) {
         if (InputUtil.isKeyPressed(window, GLFW.GLFW_KEY_F3) && InputUtil.isKeyPressed(window, ShaderReload.GLFW_KEY)) {
             ci.cancel();
